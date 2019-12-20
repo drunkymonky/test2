644c4d7a62d925d53ee3cbbb7fe0779391ff4c5e
Andrey <scorpion812@yandex.ru>
2019-12-20 21:56:06 +0300

    новая тема

---
+++ main3.py
@@ -0,0 +1,81 @@
+
+
+
+a = 10
+b = 20
+v = [x ** 2 for x in range(a,b+1)]
+#print(v)
+
+v = [x * 2 for x in v if x % 2 == 0]
+#print(v)
+
+v = [100 if x < 200 else 400 for x in v]
+#print(v)
+
+
+#1
+a = 3
+b = 7
+v =[x for x in range(a,b+1)]
+print(v)
+
+#2
+n = 32
+v = [2**x for x in range(n)]
+print(v)
+
+#3
+v  = 'bcDFRds'
+v = [x for x in v if x.isupper()]
+
+print(v)
+
+#4
+a = [
+     [1,2,3],
+     [3,5,4],
+     [6,7,9]
+    ]
+print(a[1][2])
+
+n= 4
+m = 5
+data = [[0 for y in range(m)] for i in range(n)]
+print(data)
+
+for r in data:
+    for e in r:
+        print(e, end=', ')
+    print()
+
+#5
+
+n = 5
+v = [[x*y for y in range(n)] for x in range(n)]
+print(v)
+
+
+
+
+
+"""
+#6
+v = [
+    [0,-1,0]
+    [0,0,0 ]
+    [-1,0,0]
+    ]
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+

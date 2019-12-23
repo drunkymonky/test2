73b4f14ddabfe3afd853dd666ebec74059fa03b4
Andrey <scorpion812@yandex.ru>
2019-12-23 20:33:15 +0300

    новая тема

--- main3.py
+++ main3.py
@@ -1,6 +1,6 @@
 
 
-
+"""
 # !!!!!
 result = [x+y for x in range(10) for y in range(10)]
 print(result)
@@ -68,6 +68,66 @@ for r in data:
 n = 5
 v = [[x*y for y in range(n)] for x in range(n)]
 print(v)
+
+
+"""
+# 6
+n=5
+f = [[0 for i in range(n)] for j in range(n)]
+
+f[0][1] = 'x'
+f[2][3] = 'x'
+f[2][1] = 'x'
+
+x = 1
+y = 1
+
+f.insert(0,[0]*7)
+f.append([0]*7)
+
+for i in range(1,n+1):
+    f[i].append(0)
+    f[i].insert(0,0)
+
+x+=1
+y+=1
+bomb_count = 0
+
+for i in range(-1,2):
+    for j in range(-1,2):
+        if f[x+i][y+j] == 'x':
+            bomb_count += 1
+
+#print(bomb_count)
+#print(f)
+
+result = []
+n = 4
+
+for i in range(n):
+    current_list = [x for x in range(n*i+1, n*(i+1)+1)]
+    if i % 2 == 1:
+        current_list.reverse()
+    result.append(current_list)
+
+#print(result)
+
+# 7
+x = 0
+y = 0
+dir = 0
+n =3
+result = [[0 for i in range(n)] for j in range(n)]
+i = 1
+while i < n* n:
+    
+for row in result:
+    print(row)
+
+
+print(result)
+
+
 
 
 

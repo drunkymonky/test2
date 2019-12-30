f581b3442f675db703a4a58358aa4fee2aef8168
Grisha <bobrik9494@gmail.com>
2019-12-30 21:24:09 +0300

    новая тема

---
+++ mail8.py
@@ -0,0 +1,30 @@
+
+
+class Cat():
+
+    def __init__(self, weight, name, color, age):
+        self.weight = weight
+        self.name = name
+        self.color = color
+        self.age = age
+
+    def Meow(self):
+        return self.name = "meowww..."
+
+    def is_healthy(self):
+        if self.age < self.weight:
+            return True
+        else:
+            return False
+
+barsik = Cat(name='Bars',weight=5,color='black',age=6)
+
+print(barsik.is_healthy())
+
+
+
+
+
+
+
+

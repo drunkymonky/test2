f35b66f26b2c20bcb72cdddb70664f7e6c14873d
Grisha <bobrik9494@gmail.com>
2020-01-20 21:54:15 +0300

    новая тема3

--- main13.py
+++ main13.py
@@ -1,3 +1,4 @@
+import abc
 
 class Expression():
     def __init__(self, left = None, right = None):
@@ -6,6 +7,9 @@ class Expression():
 
     def calc(self):
         return self.value
+    
+
+
 
 class Const(Expression):
     def __init__(self, value):
@@ -37,6 +41,7 @@ class M(Expression):
     def __init__(self, left = None, right = None):
         super().__init__(left, right)
 
+    @abc.abstractmethod
     def calc(self):
         return self.left.calc() / self.right.calc()
 
